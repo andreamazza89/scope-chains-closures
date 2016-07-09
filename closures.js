f165e5424942616b43fb50a175f48033f574ed6f
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-07-09 15:52:12 +0100

    Complete tutorial

---
+++ closures.js
@@ -0,0 +1,11 @@
+function foo() {
+  var bar;
+  quux = 5;
+
+  return zip;
+
+  function zip() {
+    var quux = 3;
+    bar = true;
+  }
+}
---
+++ shadows.js
@@ -0,0 +1,7 @@
+function foo() {
+  quux = 5;
+  function zip() {
+    var quux = 3;
+  }
+  var bar;
+}
